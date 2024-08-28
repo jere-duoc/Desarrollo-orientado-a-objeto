d94136e17fc14a48b4997771666b1cfdac2401c0
vina <vina@VIAWSE02LC0203.duoc.acad>
2024-08-28 14:34:42 -0400

    testing gitty git

---
+++ src/proyecto/test/git/Auto.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto.test.git;
+
+/**
+ *
+ * @author jere
+ * @Version 28-08
+ */
+public class Auto {
+    
+    private String marca,modelo,color,tipoTransmision,tipoCombustible;
+    private int año, cant_puertas;
+    private double cilindrada;
+    
+}
